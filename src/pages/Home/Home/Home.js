f126502940068b9863bf0d92cec9d5244d0bf2f4
SakibAhamedKhan <sakibahamedkhan01@gmail.com>
2022-05-08 08:55:17 +0600

    Out of Stock Item added

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Headers from '../../Shared/Headers/Headers';
 import Banner from '../Banner/Banner';
 import InventoryItems from '../InventoryItems/InventoryItems';
+import OutOfStock from '../OutOfStock/OutOfStock';
 import TotalItems from '../TotalItems/TotalItems';
 import './Home.css'
 
@@ -12,6 +13,7 @@ const Home = () => {
 			<Banner></Banner>
 			<InventoryItems></InventoryItems>
 			<TotalItems></TotalItems>
+			<OutOfStock></OutOfStock>
 		</div>
 	);
 };
